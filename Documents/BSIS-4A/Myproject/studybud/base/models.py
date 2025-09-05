a93ffa7d0499aa924b2ac790fb1cb5135b91e597
Vladimir Manumbas <mangavladimir9@gmail.com>
2025-09-05 14:16:45 +0800

    participants

--- Documents/BSIS-4A/Myproject/studybud/base/models.py
+++ Documents/BSIS-4A/Myproject/studybud/base/models.py
@@ -10,15 +10,13 @@ class Topic (models.Model):
         return self.name
     
 class Room(models.Model):
-
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic=models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    #participants = models.ManyToManyField('auth.User', related_name='rooms')
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
 
     def __str__(self):
         return self.name
